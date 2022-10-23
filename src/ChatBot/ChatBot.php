4d221e0f6e3363ab4beacda790d33d290ec13742
Christopher Hertel <mail@christopher-hertel.de>
2022-10-23 14:59:30 +0200

    fail graceful if we have no recipient

--- src/ChatBot/ChatBot.php
+++ src/ChatBot/ChatBot.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\ChatBot;
 
 use App\ChatBot\Telegram\Data\Envelope;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Notifier\Bridge\Telegram\TelegramOptions;
 use Symfony\Component\Notifier\ChatterInterface;
@@ -16,13 +18,20 @@ final class ChatBot
     public function __construct(
         private readonly ReplyMachine $replier,
         private readonly ChatterInterface $chatter,
+        private readonly LoggerInterface $logger = new NullLogger(),
     ) {
     }
 
     public function consume(Envelope $envelope): void
     {
         $reply = $this->replier->findReply($envelope);
-        $chatId = (string) $envelope->getMessage()->chat->id;
+        try {
+            $chatId = (string) $envelope->getMessage()->chat->id;
+        } catch (\RuntimeException $exception) {
+            $this->logger->error('Cannot extract message nor sender', ['exception' => $exception]);
+
+            return;
+        }
 
         $options = (new TelegramOptions())
             ->chatId($chatId)
